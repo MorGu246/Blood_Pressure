1bc8dc3cb8a0b06b62d42499816420a6bfcb18c3
MorGu246 <morgueta36@gmail.com>
2025-03-06 21:53:08 +0200

    EXPORT של בסיס הנתונים
    הקובץ הקודם היה לא נכון

--- blood.sql
+++ blood.sql
@@ -1,17 +1,21 @@
---npm i express body-parser mysql2
-
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: פברואר 23, 2024 בזמן 11:14 AM
+-- Generation Time: מרץ 06, 2025 בזמן 08:43 PM
 -- גרסת שרת: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `blood_pressure`
@@ -24,25 +28,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `blood_info` (
-                            `name` varchar(200) NOT NULL
-                            `high` int(11) NOT NULL
-                            `low` int(11) NOT NULL
-                            `pulse` int(11) NOT NULL
-                            `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `name` varchar(200) NOT NULL,
+  `high` int(11) NOT NULL,
+  `low` int(11) NOT NULL,
+  `pulse` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexes for dumped tables
+-- הוצאת מידע עבור טבלה `blood_info`
 --
 
---
--- אינדקסים לטבלה `blood_info`
---
+INSERT INTO `blood_info` (`name`, `high`, `low`, `pulse`, `date`) VALUES
+('ישראל כהן', 120, 80, 96, '2025-02-25'),
+('שמעון מזרחי', 118, 81, 97, '2025-03-03');
+COMMIT;
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `blood_info`
---
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
